cc207370fdd9fbaa3e74e729773118cff6eaf3ae
Raghavendra Rao Ananta <rananta@codeaurora.org>
2021-01-06 09:30:06 -0800

    vili: Make msm_drm module part of QGKI ramdisks
    Since msm_drm driver is created as a module by default for
    all the build configurations, it has to explicitly moved to
    first stage (vendor) ramdisk as the recovery menu needs the
    driver to display the UI. Hence, package this as a part of
    vendor ramdisk for QGKI configurations only as GKI doesn't
    have this requirement.
    
    Change-Id: Ic3d5f5a4a1d3b32de7ec4dfe9ef03bf9b8204068

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,6 +108,7 @@ BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware
 BOOT_KERNEL_MODULES := \
   fts_touch_spi.ko \
   hwid.ko \
+  msm_drm.ko \
   xiaomi_touch.ko
 
 BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(BOOT_KERNEL_MODULES)
